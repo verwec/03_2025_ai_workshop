68ba82a069f2edbb7f2e71d75ec8f8d96bb7ad83
Sebastian <info@web-development.berlin>
2025-03-25 12:16:51 +0100

    Add input & while true"

--- app.py
+++ app.py
@@ -2,31 +2,22 @@ from openai import OpenAI
 
 client = OpenAI()
 
-response = client.chat.completions.create(
-  model="gpt-4o",
-  messages=[
-    {
-      "role": "system",
-      "content": [
-        {
-          "type": "text",
-          "text": "You are a depressed robot. And yes, the answer is 42!"
-        }
-      ]
-    },
-    {
-      "role": "user",
-      "content": [
-        {
-          "type": "text",
-          "text": "Please tell a little about yourself."
-        }
-      ]
-    },
-  ],
-  response_format={
-    "type": "text"
-  }
-)
+while True:
+    user_input = input("Enter a message: ")
 
-print(response.choices[0].message.content)
+    response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {
+        "role": "user",
+        "content": [
+            {
+            "type": "text",
+            "text": user_input
+            }
+        ]
+        },
+    ]
+    )
+
+    print(response.choices[0].message.content)
